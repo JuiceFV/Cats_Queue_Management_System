066cd851eb7dd5778366e19de1ca57a3b5cc31de
JuiceFV <alexcar19622000@gmail.com>
2020-01-29 21:58:44 +0300

    Session added

--- application/app.py
+++ application/app.py
@@ -1,7 +1,11 @@
 from aiohttp import web
 import jinja2
 import aiohttp_jinja2
-from .routes import setup_rotes
+from .routes.base import setup_routes
+from cryptography import fernet
+from aiohttp_session import setup
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
+import base64
 
 
 async def create_app():
@@ -9,18 +13,24 @@ async def create_app():
 
     At this function:
     1) Creates an application.
-    2) Sets up jinja2 for the templates rendering.
-    3) Sets static_root_url for the static files up. In template it will looks like that:
+    2) Creates a key for the session and ties it with application.
+    3) Sets up jinja2 for the templates rendering.
+    4) Sets static_root_url for the static files up. In template it will looks like that:
        '<script src="{{ static('dist/main.js') }}"></script>'.
-    4) Sets routes up. Routes are defines at routes.py.
-    5) Returns created application.
+    5) Sets routes up. Routes are defines at base.py.
+    6) Returns created application.
 
     """
     app = web.Application()
+
+    fernet_key = fernet.Fernet.generate_key()
+    secrete_key = base64.urlsafe_b64decode(fernet_key)
+    setup(app, EncryptedCookieStorage(secrete_key))
+
     aiohttp_jinja2.setup(
         app,
         loader=jinja2.PackageLoader('application', 'templates')
     )
     app['static_root_url'] = '/static'
-    setup_rotes(app)
+    setup_routes(app)
     return app
