e31ea89fce1fd1416791d2fe622abb8d7c77873c
JuiceFV <alexcar19622000@gmail.com>
2020-01-29 22:03:13 +0300

    token generator class

---
+++ application/QMS/tokenqueue.py
@@ -0,0 +1,20 @@
+class Token:
+    def __init__(self):
+        self.token = None
+
+    def generate_new_token(self):
+        if self.token is None:
+            self.token = ['A', 'A', '0', '0']
+        elif self.token[3] < '9':
+            self.token[3] = chr(ord(self.token[3]) + 1)
+        elif self.token[2] < '9':
+            self.token[3] = '0'
+            self.token[2] = chr(ord(self.token[2]) + 1)
+        elif self.token[1] < 'Z':
+            self.token[3] = self.token[2] = '0'
+            self.token[1] = chr(ord(self.token[1]) + 1)
+        elif self.token[0] < 'Z':
+            self.token[3] = self.token[2] = '0'
+            self.token[1] = 'A'
+            self.token[0] = chr(ord(self.token[0]) + 1)
+        return ''.join(self.token)
