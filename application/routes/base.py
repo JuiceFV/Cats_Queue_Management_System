b915640cea863ec319ba7039b4dec1734e12b2a1
JuiceFV <alexcar19622000@gmail.com>
2020-01-29 21:58:13 +0300

    routes updated

--- application/routes/base.py
+++ application/routes/base.py
@@ -6,6 +6,7 @@ def setup_routes(app):
     app.add_routes(
                     [
                         web.get('/', frontend.Index.get),
-                        web.static('/static', 'application/static', name='static', show_index=True)
+                        web.static('/static', 'application/static', name='static', show_index=True),
+                        # web.post('/kitty', frontend.Kitty.post)
                     ]
                    )
