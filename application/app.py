4ef5b80b7950f8eb0b358faea63a8ec1c96e43bc
JuiceFV <alexcar19622000@gmail.com>
2020-04-08 23:00:17 +0300

    added comments

--- application/app.py
+++ application/app.py
@@ -39,44 +39,50 @@ async def create_app(config: dict):
     Keywords arguments:
     config:dict -- the configuration passed as argument or default configuration at "application" - directory.
 
-    At this function:
-    1) Creates an application.
-    2) Creates a key for the session and ties it with application.
-    3) Sets up jinja2 for the templates rendering.
-    4) Sets static_root_url for the static files up. In template it will looks like that:
-       '<script src="{{ static('dist/main.js') }}"></script>'.
-    5) Sets routes up. Routes are defines at base.py.
-    6) Configures database
-    7) Closes all connections with database
-    8) Returns created application.
-
+    returns created application
     """
+
+    # Creating an application
     app = web.Application()
 
+    # Importing a config into the application
+    # Also creating the global variable responsible for a token
     app['config'] = config
     app['new_token'] = TokenGenerator()
 
     # Adding logging
+    # The only option where logs are invisible is 'release'
     if app['config']['run_type'] != 'release':
         logging.basicConfig(level=logging.DEBUG)
 
+    # Adding key specifying an each session
     fernet_key = fernet.Fernet.generate_key()
     secrete_key = base64.urlsafe_b64decode(fernet_key)
+
+    # Setting up this key for the application
     setup(app, EncryptedCookieStorage(secrete_key))
 
+    # Setting template loader
     aiohttp_jinja2.setup(
         app,
         loader=jinja2.PackageLoader('application', 'templates')
     )
 
+    # Setting up the route for the static files.
     app['static_root_url'] = '/static'
 
+    # Setting up the routes specified at ./routes/base.py
     setup_routes(app)
 
+    # Setting up the routes specified at ./middlewares/mw.py
     setup_middlewares(app)
 
+    # Uploading and starting a database on application starts
     app.on_startup.append(on_start)
-    # ADD close remains tasks on_cleanup
+
+    # TODO ADD close remains tasks on_cleanup
+
+    # Cleaning up a database on application shutdown.
     app.on_cleanup.append(on_shutdown)
 
     return app
