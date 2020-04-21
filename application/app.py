c45a020db6bbd1a74f8cd2843283eeca96be93f4
JuiceFV <alexcar19622000@gmail.com>
2020-04-21 23:51:08 +0300

    Added run-type processing and global/app variables creation

--- application/app.py
+++ application/app.py
@@ -1,17 +1,6 @@
 """Here is placed the function which configures an application.
 
-Import packages:
-jinja2 --
-bas64 --
-logging --
-aiohttp_jinja2 --
-aiohttp.web --
-cryptography.fernet --
-aiohttp_session.setup --
-.routes.base.setup_routes --
-.QMS.tokengenerator.TokenGenerator --
-.database.base.on_start/on_shutdown --
-aiohttp_session.cookie_storage.EncryptedCookieStorage --
+
 
 Functions:
 create_app(config: dict) --
@@ -33,6 +22,56 @@ from .database import on_start, on_shutdown
 from aiohttp_session.cookie_storage import EncryptedCookieStorage
 
 
+def detecting_runtype(app):
+    """This function checks for the correct run-type.
+    There are 3 possible run-types: debug/release/test
+
+    Keywords arguments:
+    app -- the application
+    """
+
+    # If a run-type doesn't coincide with 3 following run-types then we're raising custom exception.
+    if app['config']['run_type'] != 'release' and \
+       app['config']['run_type'] != 'debug' and \
+       app['config']['run_type'] != 'test':
+        try:
+            raise Exception('Wrong run-type', app['config']['run_type'])
+        except Exception as inst:
+            error, user_input = inst.args
+            print(f'{error}: You passed {user_input} as run-type.')
+            raise
+
+    # If the we deploy the application then we turn off logging
+    if app['config']['run_type'] != 'release':
+        logging.basicConfig(level=logging.DEBUG)
+
+
+def setup_application_variables(app, config):
+    """This function sets up global variables (global for the app)
+
+    Keywords arguments:
+    app -- the application
+    config -- a configuration extracted form config-file
+    """
+
+    # Setting configuration
+    app['config'] = config
+
+    # Setting variable responsible for in-time queue representation on web page.
+    app['request_for_queue_update_vis'] = False
+
+    # Here we'ill contain the ban-list
+    # index==0 - the file where ip' are placed constantly
+    # index==1 - the dictionary where ip' are placed temporary (Because, Dict-Find complexity is O(1))
+    app['ban_list'] = [open(".ip_banlist", 'r')]
+    app['ban_list'].append({ip.strip(): 'banned' for ip in app['ban_list'][0]})
+
+    # A token, specifically novel generated
+    app['new_token'] = TokenGenerator()
+
+    app['redundant_tokens_vis'] = [False, []]
+
+
 async def create_app(config: dict):
     """Creates an application with routes and returns it.
 
@@ -48,15 +87,10 @@ async def create_app(config: dict):
     # Importing a config into the application
     # Then creating the global variable responsible for queue representation, specifically auto-removing
     # Also creating the global variable responsible for a token
-    app['config'] = config
-    app['update_queue_vis'] = False
-    app['ban_list'] = {}
-    app['new_token'] = TokenGenerator()
+    setup_application_variables(app, config)
 
-    # Adding logging
-    # The only option where logs are invisible is 'release'
-    if app['config']['run_type'] != 'release':
-        logging.basicConfig(level=logging.DEBUG)
+    # It's checking for a run-type. There are ONLY 3 options debug/release/test
+    detecting_runtype(app)
 
     # Adding key specifying an each session
     fernet_key = fernet.Fernet.generate_key()
