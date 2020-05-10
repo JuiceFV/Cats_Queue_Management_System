fc23d50a9db8996df83a5eb003ec2f1954f5f9b7
JuiceFV <alexcar19622000@gmail.com>
2020-05-10 21:04:31 +0300

    added delete_token_from_db to __init__

--- application/database/__init__.py
+++ application/database/__init__.py
@@ -1,7 +1,9 @@
 """The init.py for 'database' which contains the only permit for function's sharing.
 """
 
-from .base import on_start, insert_token_into_db, on_shutdown, db_empty, get_all_tokens, get_num_of_tokens
+from .base import on_start, insert_token_into_db, on_shutdown, db_empty, get_all_tokens, get_num_of_tokens,\
+    delete_token_from_db
 
 # Admitting permit for usage in other files to the the following functions.
-__all__ = ('on_start', 'insert_token_into_db', 'on_shutdown', 'db_empty', 'get_all_tokens', 'get_num_of_tokens')
+__all__ = ('on_start', 'insert_token_into_db', 'on_shutdown', 'db_empty', 'get_all_tokens', 'get_num_of_tokens',
+           'delete_token_from_db',)
