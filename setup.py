e114f4c47e674174315806fa13b8e42c05b1b9d3
JuiceFV <aleksandr.juicefv@gmail.com>
2020-05-19 15:48:43 +0300

    removed some redundant commentaries

--- setup.py
+++ setup.py
@@ -7,20 +7,6 @@ from setuptools import setup, find_packages
 # and distribute python packages. You can find more information here:
 # https://setuptools.readthedocs.io/en/latest/setuptools.html
 
-# Following statements are represent the brief description:
-# 1) name - sets the project name.
-# 2) version - sets the project version
-# 3) description - this is the short description of your project.
-# 4) author - Author name/nickname
-# 5) author_email - Author contact e-mail.
-# 6) packages - all packages used in the project. Apparently, find packages - looking for them. More information here:
-#    https://setuptools.readthedocs.io/en/latest/setuptools.html#using-find-packages
-# 7) zip_safe - For some use cases, Python packages may be run directly from a zip file. More information here:
-#    https://setuptools.readthedocs.io/en/latest/setuptools.html#setting-the-zip-safe-flag
-# 9) entry_points - sets the entry point of the whole application.
-# 9.1) console_scripts - means that you can write the 'start_app' instead of the 'python3 entry_py'
-#      and the application starts
-
 
 setup(
     name="kitty_getter",
