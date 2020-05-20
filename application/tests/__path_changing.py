996c2feb521a1843d4c54304d9adf10ff0a65429
JuiceFV <aleksandr.juicefv@gmail.com>
2020-05-20 23:01:58 +0300

    config umodified for docker

--- application/tests/__path_changing.py
+++
@@ -1,8 +0,0 @@
-"""Due to I was having huge troubles with tests launching from command line. I appends the project directory to the
-exploring paths. If pycharm underlines the import of this file, do not worry it is ordinary behavior.
-"""
-import sys
-import os
-
-sys.path.append(os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..')))
